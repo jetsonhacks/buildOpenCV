a0addc16b23effbe0670d2fb9d2d18f207a4ecf8
jetsonhacks <jim@jetsonhacks.com>
2015-08-11 20:34:26 -0700

    Add dependencies

--- buildOpenCV.sh
+++ buildOpenCV.sh
@@ -6,6 +6,14 @@
 # This version is for CUDA 6.5
 # OpenCV 2.4.10
 # Note: Earlier versions of OpenCV (< 2.4.10) are not compatible with CUDA 6.5
+
+# Get opencv dependencies
+apt-get -y install \
+  checkinstall git cmake libfaac-dev libjack-jackd2-dev \
+  libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libsdl1.2-dev \
+  libtheora-dev libva-dev libvdpau-dev libvorbis-dev libx11-dev libxfixes-dev \
+  libxvidcore-dev texi2html yasm zlib1g-dev
+
 NUM_THREADS=4
 ver=2.4.10
 
