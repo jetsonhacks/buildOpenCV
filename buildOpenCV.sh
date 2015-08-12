7f6a293d03b790ded5eb679755a802322a3f4cd4
jetsonhacks <jim@jetsonhacks.com>
2015-08-11 20:49:18 -0700

    Arrange dependencies

--- buildOpenCV.sh
+++ buildOpenCV.sh
@@ -7,12 +7,27 @@
 # OpenCV 2.4.10
 # Note: Earlier versions of OpenCV (< 2.4.10) are not compatible with CUDA 6.5
 
-# Get opencv dependencies
-apt-get -y install \
-  checkinstall git cmake libfaac-dev libjack-jackd2-dev \
+sudo apt-add-repository universe
+sudo apt-add-repository multiverse
+sudo apt-get update
+
+# Install OpenCV dependencies
+sudo apt-get -y install \
+  build-essential checkinstall cmake pkg-config yasm
+
+# Video I/O
+sudo apt-get -y install \
+  libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev
+
+# For building Python wrappers
+sudo apt-get -y install \
+  python-dev python-numpy
+
+sudo apt-get -y install \
+  libfaac-dev libjack-jackd2-dev \
   libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libsdl1.2-dev \
   libtheora-dev libva-dev libvdpau-dev libvorbis-dev libx11-dev libxfixes-dev \
-  libxvidcore-dev texi2html yasm zlib1g-dev
+  libxvidcore-dev texi2html zlib1g-dev
 
 NUM_THREADS=4
 ver=2.4.10
@@ -25,5 +40,5 @@ cd build
 # Jetson architecture is 3.2
 cmake -D CUDA_ARCH_BIN=3.2 ..
 make -j$NUM_THREADS
-make -j$NUM_THREADS install
+sudo make -j$NUM_THREADS install
 /bin/echo -e "\e[1;32mOpenCV build complete.\e[0m"
